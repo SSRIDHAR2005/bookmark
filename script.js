06689470e2a1b482c9ca6b2d60c3c38f33f153d6
Sridhar <139063533+SSRIDHAR2005@users.noreply.github.com>
2025-03-29 12:45:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ const firebaseConfig = {
   
 
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 // Function to load bookmarks from Firestore
